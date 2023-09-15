1f795ecb9af7b609734f2a352e4d9defd39b38bd
a-matyukh <amatyukh@gmail.com>
2023-09-15 13:19:00 +0300

    less threshold and score drawing

--- src/Report/draw_detections.js
+++ src/Report/draw_detections.js
@@ -10,16 +10,21 @@ async function draw_detections(snapshot, isDanger) {
     for (const person of snapshot.detections) {
         // const color = person.isIntersect ? "yellow" : "aqua"
         const color = "aqua"
-        draw_box(ctx, person.bbox, color)
+        draw_box(ctx, person.bbox, color, person.score)
     }
     snapshot.buffer = await canvas.encode('jpeg', 50)
     return snapshot
 }
-function draw_box(ctx, rect, color) {
+function draw_box(ctx, rect, color, score) {
     const [x, y, width, height] = rect
     ctx.lineWidth = 10
     ctx.strokeStyle = color
     ctx.strokeRect(x, y, width, height)
+    if (score) {
+        ctx.font = "48px serif"
+        ctx.fillStyle = "yellow"
+        ctx.fillText(`${Math.floor(score * 100)}`, x - 20, y - 20)
+    }
 }
 
 module.exports = draw_detections
