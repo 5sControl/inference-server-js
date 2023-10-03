4e3ebb9d9614b62722939ec4571e40a2648a577b
a-matyukh <amatyukh@gmail.com>
2023-10-03 21:30:42 +0300

    chore: write debugDB only at 9-10

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -40,7 +40,7 @@ function is_working_time() {
     }
 
     const start_work_day = convert_time_to_minutes('09:00')
-    const finish_work_day = convert_time_to_minutes('21:00')
+    const finish_work_day = convert_time_to_minutes('10:00')
 
     const date = new Date()
     const hour = (date.getHours() < 10 ? '0' : '' ) + date.getHours().toString()
